2c784223f9aa7681474f08d87e026930e652a463
cass <alicastilo1097@gmail.com>
2023-07-28 20:17:22 +0300

    create the drawer

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -4,28 +4,79 @@ import React from 'react'
 // import Toolbar from '@mui/material/Toolbar';
 // import Typography from '@mui/material/Typography';
 // import Button from '@mui/material/Button';
-import { Avatar, Link, Typography, Toolbar, AppBar } from '@mui/material';
+import { Avatar, Link, Typography, Toolbar, AppBar, Divider, Drawer } from '@mui/material';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const drawerWidth = 240;
+
 
 const Root = () => {
+
+
+
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar>
 
 
 
 
-          <Link  sx={{ flexGrow: 1, textDecoration: "none", "&:hover" : {fontSize: "22px"}, transition: "0.2s" }} color="inherit" href="/"> My Expenses</Link>
 
-<Typography variant="h6" color="inherit" >Castillo</Typography>
-
-<Avatar sx={{ml: "10px", width: 35, height: 35}} alt="Cemy Sharp" src="./public/images/Untitled-4.jpg" />
-
-
-
-          
+      <AppBar position="static" sx={{ ml : `${drawerWidth}px`, width : `calc(100% - ${drawerWidth}px )` }}>
+        <Toolbar >
+          <Link  sx={{ flexGrow: 1, textDecoration: "none", "&:hover" : {fontSize: "22px"}, transition: "0.2s" }} color="inherit" href="/"> My Expenses ddddddddddddddddddddddddddddddddddddddddddddddd</Link>
+          <Typography variant="h6" color="inherit" >Castillo </Typography>
+          <Avatar sx={{ml: "10px", width: 35, height: 35}} alt="Cemy Sharp" src="./public/images/Untitled-4.jpg" />
         </Toolbar>
       </AppBar>
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        
+        {/* <Divider /> */}
+      </Drawer>
+
+
+
+
     </div>
   )
 }
