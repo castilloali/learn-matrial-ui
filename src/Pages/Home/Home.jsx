24cf80ea23b9a7e87188300df1ad904092b14a83
cass <alicastilo1097@gmail.com>
2023-08-03 13:21:15 +0300

    create paper

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,39 @@
+import { Close } from "@mui/icons-material";
+import { Box, Paper, Typography, IconButton } from "@mui/material";
 
 const Home = () => {
-
   return (
-<>
-<div>
- <h2>cdddddddddd</h2>
- <h2>cdddddddddd</h2>
- <h2>cdddddddddd</h2>
- <h2>cdddddddddd</h2>
- <h2>cdddddddddd</h2>
-</div>
-</>
-  )
-}
+    <Box>
+      <Paper
+      elevation={3} 
+        sx={{
+          width: "366px",
+          display: "flex",
+          justifyContent: "space-between",
+          mt: "22px",
+          pt: "20px",
+          pb: "7px",
+          pl: "15px",
+          position: "relative",
+        }}
+      >
+        <Typography variant="h6" color="initial">
+          GYM
+        </Typography>
+        <Typography
+          sx={{ mr: "33px", opacity: ".7" }}
+          variant="h6"
+          color="initial"
+        >
+          $100
+        </Typography>
+        <IconButton sx={{ position: "absolute", top: "0", right: "0"}}>
+          <Close fontSize="small"/>
+        </IconButton>
+      </Paper>
 
-export default Home
+    </Box>
+  );
+};
+
+export default Home;
